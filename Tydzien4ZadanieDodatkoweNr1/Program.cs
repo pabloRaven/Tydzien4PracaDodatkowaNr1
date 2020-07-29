e1c95f2572bd2a7a9ced52f04d67bc8fdb9cfefd
Pawel <you@example.com>
2020-07-29 21:59:19 +0200

    first commit

---
+++ Tydzien4ZadanieDodatkoweNr1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien4ZadanieDodatkoweNr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ciąg dowolnych liczb całkowitych odzielonych spacją");
+            
+            var myList = new List<int>();
+            var min = myList.Min();
+            var max = myList.Max();
+            Console.WriteLine($"Najmniejsza liczba to : {min} a największa to : {max} ");
+        }
+    }
+}
